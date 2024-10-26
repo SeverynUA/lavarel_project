1d643a9f322708496d9da79b80ef5b89484de232
SeverynUA <123012574+SeverynUA@users.noreply.github.com>
2024-10-25 18:34:17 -0700

    qweeqwqwe
    123

---
+++ resources/views/ExtraInfo.blade.php
@@ -0,0 +1,6 @@
+<h1>ExtraInfo - Інформація про мене</h1>
+<p>Додаткова інформація про мої проєкти...</p>
+<a href="{{ route('MyProfile') }}" class="text-blue-500 hover:underline">Перейти на MyProfile</a>
+<div class="mt-2">
+<a href="{{ route('dashboard') }}" class="text-blue-500 hover:underline">Перейти на головну</a>
+</div>
